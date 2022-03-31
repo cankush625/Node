abb36aaf3e25eb838f84202ec16884ef66e8d109
cankush625 <cankush625@gmail.com>
2022-03-31 22:57:59 +0530

    feat(Init): Initial commit
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ globals.js
@@ -0,0 +1,11 @@
+// GLOBALS - NO WINDOW
+
+// global - global object that available throughout the app
+// __dirname - path to current directory
+// __filename - filename
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about the env where the program is being executed
+
+console.log(__dirname)
+console.log(__filename)

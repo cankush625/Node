9ea09dd2cdedf35b6c1cfab5138e434137be941d
cankush625 <cankush625@gmail.com>
2022-04-04 22:38:33 +0530

    feat(events): Events Emmiter
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- events/events_emitter.js
+++ events/events_emitter.js
@@ -1,3 +1,9 @@
+// Note:
+// Event is core building block of NodeJS. Lots of node modules extends/use the Events Emitter.
+// So, even if we are not using Event Emitter directly, it is being used behind
+// the scene by some modules we are using.
+// Eg. .on method from HTTP module. http.Server has events like request, connect, etc.
+
 const EventsEmitter = require("events");
 
 const customEmitter = new EventsEmitter();
@@ -6,4 +12,11 @@ customEmitter.on("response", () => {
     console.log("Date received");
 });
 
-customEmitter.emit("response")
+customEmitter.emit("response");
+
+// Event with arguments
+customEmitter.on("welcome", (name) => {
+    console.log(`Hello ${name}`);
+});
+
+customEmitter.emit("welcome", "Robert");
